8407d6f063dac2977c05e7a69bcb56a074d22492
Jie Bo Ti <tijiebo@hotmail.com>
2018-07-13 15:27:46 +0800

    First commit

---
+++ app/src/main/java/com/dcube/devchap/spinarbottle/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dcube.devchap.spinarbottle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
